af961b216f9b139ff301354b0198a82ffd175283
Anthony <AnthonyJ.Parchman@Gmail.com>
2018-11-19 21:12:36 -0600

    Basic initialization of Project
    added core class and started on interview

---
+++ src/game/Core.java
@@ -0,0 +1,19 @@
+package game;
+
+public class Core {
+
+	public static void main(String[] args) {
+		/* TODO plico is a game
+		 * Player gets interview
+		 * Player Gets a number of chips
+		 * Player selects location to drop chip
+		 * Player Drops a chip
+		 * Chip falls
+		 * Rewards are given
+		 */
+		Interview.InitTest();
+		Interview.AskName();
+		
+	}
+
+}
---
+++ src/game/Interview.java
@@ -0,0 +1,21 @@
+package game;
+
+import java.util.Scanner;
+
+public class Interview {
+	/* 
+	not completely sure what these do, they are fields
+	still reading documentation 
+	public static Object InitTest;
+	public static Object AskName;
+	 */
+	//just a quick print to test functionality
+	public static void InitTest() {
+		System.out.print("Hello ");	
+	}
+	//scanners time
+	public static void AskName() {
+	System.out.print("What is your name.");
+	Scanner sc = new Scanner(System.in); 
+	}
+}
